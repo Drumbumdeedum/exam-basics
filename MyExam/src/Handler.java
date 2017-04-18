1526749da94da98ec80dcaeeaada703537cc500c
Daniel Szalay <sza.dan.uj@gmail.com>
2017-04-18 12:29:59 +0200

    add first working version of Copy excersize

---
+++ MyExam/src/Handler.java
@@ -0,0 +1,39 @@
+public class Handler {
+  ReadAndWrite copyMechanism;
+
+
+  public Handler() {
+     copyMechanism = new ReadAndWrite();
+  }
+
+  public void handleCommands(String[] args) {
+    try {
+      if (args.length == 0) {
+        printUsage();
+      } else if (args.length == 1) {
+        noDestination();
+      } else if (args.length > 2) {
+        tooManyArguments();
+        printUsage();
+      } else if (args.length == 2) {
+        copyMechanism.readWrite(args[0], args[1]);
+      }
+    } catch(Exception e) {
+      System.out.println("Woops, and error has occurred!");
+    }
+  }
+
+  public void printUsage() {
+    System.out.println("copy [source] [destination]");
+  }
+
+  public void noDestination() {
+    System.out.println("No destination provided");
+  }
+
+  public void tooManyArguments() {
+    System.out.println("Too many arguments provided, follow usage guidelines:");
+  }
+}
+
+
---
+++ MyExam/src/ReadAndWrite.java
@@ -0,0 +1,21 @@
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class ReadAndWrite {
+
+  public void readWrite(String fromFile, String toFile) {
+    try {
+      Path sourceFile = Paths.get(fromFile);
+      Path destinationFile = Paths.get(toFile);
+
+      List<String> lines = Files.readAllLines(sourceFile);
+      Files.write(destinationFile, lines, Charset.defaultCharset());
+
+    } catch (Exception e) {
+      System.out.println("Woops, and error has occurred!");
+    }
+  }
+}
